0bd05128f3ff7428d4c9b0998e1296be05526106
Jason Fadikpe <jfadikpe2021@elam.co.uk>
2021-11-10 12:33:22 +0000

    character control new phys

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
-{   float maxSpeed = 1.0f;
+{   float maxSpeed = 5.0f;
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
+    Rigidbody myRigidBody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
     
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.AddForce(transform.up * jumpForce);
+        }
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
