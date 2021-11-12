b9e5ff6b962fe8ee451645de8f520a91fc8fb137
Pewners <jfadikpe2021@elam.co.uk>
2021-11-12 18:46:58 +0000

    added sprint

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
-{   float maxSpeed = 5.0f;
+{   
+	public float maxSpeed;
+	public float normalSpeed = 10.0f;
+	public float sprintSpeed = 40.0f;
+	public float maxSprint = 5.0f;
+	float sprintTimer;
+
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
@@ -18,7 +24,11 @@ public class CharacterController : MonoBehaviour
     
     void Start()
     {
-        cam = GameObject.Find("Main Camera");
+        Cursor.lockState = CursorLockMode.Locked;
+		
+		sprintTimer = maxSprint;
+		
+		cam = GameObject.Find("Main Camera");
         myRigidBody = GetComponent<Rigidbody>();
     }
 
@@ -32,7 +42,22 @@ public class CharacterController : MonoBehaviour
             myRigidBody.AddForce(transform.up * jumpForce);
         }
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+		if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+		{
+			maxSpeed = sprintSpeed;
+			sprintTimer = sprintTimer - Time.deltaTime;
+		} else
+		{
+			maxSpeed = normalSpeed;
+			if (Input.GetKey(KeyCode.LeftShift) == false)
+			{
+				sprintTimer = sprintTimer + Time.deltaTime;
+			}
+		}
+
+		sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis ("Horizontal") * maxSpeed));
         myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
