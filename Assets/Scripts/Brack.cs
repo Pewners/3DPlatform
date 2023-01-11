331ce8c030f39cff4030081e917ab07843ce23a4
Pewners <jfadikpe2021@elam.co.uk>
2023-01-11 15:19:06 +0000

    camera

--- Assets/Scripts/Brack.cs
+++ Assets/Scripts/Brack.cs
@@ -7,26 +7,34 @@ public class Brack : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-    //public Rigidbody myRigidBody;
+    public Rigidbody myRigidBody;
 
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    public float jumpForce = 500.0f;
     public bool isOnGround;
+    public GameObject groundChecker;
     public LayerMask groundLayer;
+    public float jumpForce = 500.0f;
 
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+        //isOnGround = controller.isGrounded;
+        //if (isOnGround && velocity.y < 0)
+        //{
+        //    velocity.y = 0f;
+        //}
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >=0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -36,15 +44,26 @@ public class Brack : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            //myAnim.SetTrigger("jumped");
-            AddForce(transform.up * jumpForce);
-        }
+
+
+        //if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    myRigidBody.AddForce(transform.up * jumpForce);
+        //}
     }
 
-    private void AddForce(Vector3 vector3)
-    {
-        throw new NotImplementedException();
-    }
+    //public float jumpForce = 1.0f;
+    //public float gravity = -9.81f;
+    //float velocity;
+
+    //void FixedUpdate()
+    //{
+    //    velocity += gravity * Time.deltaTime;
+    //    if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+    //    {
+    //        velocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravity);
+    //    }
+    //    transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
+    //}
+
 }
